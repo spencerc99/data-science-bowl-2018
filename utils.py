4782bc1dec106ff664bdc59cb2b097f967d696b0
Spencer Chang <spencerc99@gmail.com>
2018-03-20 18:38:26 -0500

    fixed some utils stuff to fit and u-net almost ready to train

--- utils.py
+++ utils.py
@@ -8,7 +8,6 @@ import pandas as pd
 import random
 from collections import defaultdict
 from PIL import Image
-import image
 from skimage.color import rgb2gray
 
 # Some constants
@@ -19,7 +18,6 @@ INPUT_FOLDER = TRAIN_FOLDER + 'input/'
 LABELS_FOLDER = TRAIN_FOLDER + 'labels/'
 patients = os.listdir(INPUT_FOLDER)
 patients.sort()
-print(patients)
 
 from skimage.color import rgb2gray
 
@@ -58,7 +56,7 @@ def show_image_and_masks(patient):
 
 
 def resize_images(img):
-    resizedimg = transform.resize(img, (256, 256))
+    resizedimg = transform.resize(img, (256, 256, 3))
     return resizedimg
 
 def load_input_image(patient, inp_folder=INPUT_FOLDER):
@@ -94,22 +92,58 @@ def show_image_and_masks(patient):
     plt.subplot(212)
     plt.imshow(resized_cmask)
 
-def get_resized_train_test():
+def get_resized_train_data():
     # use this and then split stuff into validation
-    input_x = []
-    input_y = []
+    input_x = np.zeros((len(patients), 256, 256, 3))
+    input_y = np.zeros((len(patients), 256, 256, 3))
     train_data = int(len(patients) * .8)
     for i in range(train_data):
         img = resize_images(load_input_image(patients[i]))
-        input_x.append(img)
+        input_x[i] = img
         yimg = resize_images(get_composed_masks_img(patients[i]))
-        input_y.append(yimg)
-        if img.shape != (256, 256):
-            print "x dimensions wrong"
-            print img.shape
-            print i
-        if yimg.shape != (256, 256):
-            print "y dimensions wrong"
-            print yimg.shape
-            print i
+        input_y[i] = yimg
+        # if img.shape != (256, 256):
+        #     print("x dimensions wrong")
+        #     print(img.shape)
+        #     print(i)
+        # if yimg.shape != (256, 256):
+        #     print("y dimensions wrong")
+        #     print(yimg.shape)
+        #     print(i)
+    
     return input_x, input_y
+
+def rle_encoding(x):
+    '''
+    x: numpy array of shape (height, width), 1 - mask, 0 - background
+    Returns run length as list
+    '''
+    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return " ".join([str(i) for i in run_lengths])
+
+# print('RLE Encoding for the current mask is: {}'.format(rle_encoding(label_mask)))
+
+def analyze_image(patient, predict):
+    img = load_input_image(patient, inp_folder=TEST_FOLDER)
+    segmented_img = predict(img)
+    rle = rle_encoding(segmented_img)
+    s = pd.Series({'ImageId': patient, 'EncodedPixels': rle})
+    im_df = pd.DataFrame()
+    im_df = im_df.append(s, ignore_index=True)
+    return im_df
+
+def analyze_list_of_images(patients, predict_method):
+    '''
+    Takes a list of image paths (pathlib.Path objects), analyzes each,
+    and returns a submission-ready DataFrame.'''
+    all_df = pd.DataFrame()
+    for patient in patients:
+        im_df = analyze_image(patient, predict_method)
+        all_df = all_df.append(im_df, ignore_index=True)
+    return all_df
