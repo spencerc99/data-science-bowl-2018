222b16eb54f7c0e0ccafc59cf80648a241362f2c
Spencer Chang <spencerc99@gmail.com>
2018-03-21 10:08:41 -0500

    fixed sh script to zip in proper directory

--- get_data.sh
+++ get_data.sh
@@ -8,7 +8,8 @@ mkdir data/train/labels
 pip install kaggle
 kaggle competitions download -c data-science-bowl-2018
 cp ~/.kaggle/competitions/data-science-bowl-2018/stage1_train.zip data/train/input/
-unzip data/train/input/stage1_train.zip
-cp ~/.kaggle/competitions/data-science-bowl-2018/stage1_train_labels.csv data/train/labels/
+unzip data/train/input/stage1_train.zip -d data/train/input/
+cp ~/.kaggle/competitions/data-science-bowl-2018/stage1_train_labels.csv.zip data/train/labels/
+unzip data/train/labels/stage1_train_labels.csv.zip -d data/train/labels/
 cp ~/.kaggle/competitions/data-science-bowl-2018/stage_1_test.zip data/test/
-unzip data/test/stage_1_test.zip
+unzip data/test/stage_1_test.zip -d data/test/
