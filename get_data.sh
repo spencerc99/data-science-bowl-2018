2e650419ba4068fe7146432e2cd3eddcefebe488
Spencer Chang <spencerc99@gmail.com>
2018-03-21 00:56:20 -0500

    fixed sh script

--- get_data.sh
+++ get_data.sh
@@ -7,8 +7,8 @@ mkdir data/train/input
 mkdir data/train/labels
 pip install kaggle
 kaggle competitions download -c data-science-bowl-2018
-mv stage1_train.zip data/train/input/
+cp ~/.kaggle/competitions/data-science-bowl-2018/stage1_train.zip data/train/input/
 unzip data/train/input/stage1_train.zip
-mv stage1_train_labels.csv data/train/labels/
-mv stage_1_test.zip data/test/
+cp ~/.kaggle/competitions/data-science-bowl-2018/stage1_train_labels.csv data/train/labels/
+cp ~/.kaggle/competitions/data-science-bowl-2018/stage_1_test.zip data/test/
 unzip data/test/stage_1_test.zip
