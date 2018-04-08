2931c381d3258152f8d509e8b4762f9e8de29d1e
ameeshshah <ars7@rice.edu>
2018-04-08 20:52:34 +0000

    couple changes

--- models/unet.py
+++ models/unet.py
@@ -11,75 +11,75 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from tqdm import tqdm
 from constants import *
 from utils import mean_iou, get_resized_train_data
-from models.basic_model import BasicModel
+from basic_model import BasicModel
 
 
 class UNet(BasicModel):
     def __init__(self, config):
         super().__init__(config)
         inps = Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
-        conv1 = Conv2D(64, (3, 3), activation='relu',
+        conv1 = Conv2D(8, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(inps)
         conv1 = Dropout(.1)(conv1)
-        conv1 = Conv2D(64, (3, 3), activation='relu',
+        conv1 = Conv2D(8, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv1)
         pool = MaxPooling2D(pool_size=(2, 2))(conv1)
-        conv2 = Conv2D(128, (3, 3), activation='relu',
+        conv2 = Conv2D(16, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(pool)
         conv2 = Dropout(.1)(conv2)
-        conv2 = Conv2D(128, (3, 3), activation='relu',
+        conv2 = Conv2D(16, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv2)
         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-        conv3 = Conv2D(256, (3, 3), activation='relu',
+        conv3 = Conv2D(32, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(pool2)
         conv3 = Dropout(.2)(conv3)
-        conv3 = Conv2D(256, (3, 3), activation='relu',
+        conv3 = Conv2D(32, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv3)
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-        conv4 = Conv2D(512, (3, 3), activation='relu',
+        conv4 = Conv2D(64, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(pool3)
         conv4 = Dropout(.2)(conv4)
-        conv4 = Conv2D(512, (3, 3), activation='relu',
+        conv4 = Conv2D(64, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv4)
         pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-        conv5 = Conv2D(1024, (3, 3), activation='relu',
+        conv5 = Conv2D(128, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(pool4)
         conv5 = Dropout(.2)(conv5)
-        conv5 = Conv2D(1024, (3, 3), activation='relu',
+        conv5 = Conv2D(128, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv5)
 
         # start deconvoluting
         deconv1 = Conv2DTranspose(
-            512, (2, 2), strides=(2, 2), padding='same')(conv5)
+            64, (2, 2), strides=(2, 2), padding='same')(conv5)
         deconv1 = concatenate([deconv1, conv4])
-        conv6 = Conv2D(512, (3, 3), activation='relu',
+        conv6 = Conv2D(64, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(deconv1)
         conv6 = Dropout(.2)(conv6)
-        conv6 = Conv2D(512, (3, 3), activation='relu',
+        conv6 = Conv2D(64, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv6)
         deconv2 = Conv2DTranspose(
-            256, (2, 2), strides=(2, 2), padding='same')(conv6)
+            32, (2, 2), strides=(2, 2), padding='same')(conv6)
         deconv2 = concatenate([deconv2, conv3])
-        conv7 = Conv2D(256, (3, 3), activation='relu',
+        conv7 = Conv2D(32, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(deconv2)
         conv7 = Dropout(.2)(conv7)
-        conv7 = Conv2D(256, (3, 3), activation='relu',
+        conv7 = Conv2D(32, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv7)
         deconv3 = Conv2DTranspose(
-            128, (2, 2), strides=(2, 2), padding='same')(conv7)
+            16, (2, 2), strides=(2, 2), padding='same')(conv7)
         deconv3 = concatenate([deconv3, conv2])
-        conv8 = Conv2D(128, (3, 3), activation='relu',
+        conv8 = Conv2D(16, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(deconv3)
         conv8 = Dropout(.2)(conv8)
-        conv8 = Conv2D(128, (3, 3), activation='relu',
+        conv8 = Conv2D(16, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv8)
         deconv4 = Conv2DTranspose(
-            64, (2, 2), strides=(2, 2), padding='same')(conv8)
+            8, (2, 2), strides=(2, 2), padding='same')(conv8)
         deconv4 = concatenate([deconv4, conv1])
-        conv9 = Conv2D(64, (3, 3), activation='relu',
+        conv9 = Conv2D(8, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(deconv4)
         conv9 = Dropout(.2)(conv9)
-        conv9 = Conv2D(64, (3, 3), activation='relu',
+        conv9 = Conv2D(8, (3, 3), activation='relu',
                        padding='same', kernel_initializer='he_normal')(conv9)
         outs = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
 
@@ -94,3 +94,7 @@ class UNet(BasicModel):
                                        save_best_only=True)
         return self.model.fit(X_train, y_train, validation_split=0.1, batch_size=16, epochs=50,
                                  callbacks=[earlystopper, checkpointer])  # Define IoU metric
+
+if __name__ == "__main__":
+    unet = UNet({"model_name" : "spicyboost", "random_seed" : 1, "max_iter" : 500000})
+    unet.train()
