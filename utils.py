f4bb1c1a99b0706c2caebb9d7e5b437c0c197c8d
Spencer Chang <spencerc99@gmail.com>
2018-04-09 21:54:53 -0500

    dice coeff actually

--- utils.py
+++ utils.py
@@ -232,3 +232,15 @@ def mean_iou(y_true, y_pred):
             score = tf.identity(score)
         prec.append(score)
     return K.mean(K.stack(prec), axis=0)
+
+
+# Metric function
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+# Loss funtiont
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
